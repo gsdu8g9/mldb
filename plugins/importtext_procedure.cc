3713cbda62eaa4b57f64ddd168a1a194a27b6700
Jeremy Barnes <jeremy@datacratic.com>
2017-03-12 21:25:29 +0000

    Use multi-threaded RE2 for SkipLine regex (MLDB-2168)
    Conflicts:
    	plugins/importtext_procedure.cc

--- plugins/importtext_procedure.cc
+++ plugins/importtext_procedure.cc
@@ -23,6 +23,7 @@
 #include "mldb/utils/progress.h"
 #include "mldb/jml/utils/vector_utils.h"
 #include "mldb/utils/log.h"
+#include "mldb/ext/re2/re2/re2.h"
 
 
 using namespace std;
@@ -759,6 +760,9 @@ struct ImportTextProcedureWorkInstance
 
         encoding = parseEncoding(config.encoding);
 
+        RE2 skipLineRegex(config.skipLineRegex.initialized()
+                          ? config.skipLineRegex.surface().rawData()
+                          : "");
 
         if (isTextLine) {
             //MLDB-1312 optimize if there is no delimiter: only 1 column
@@ -799,6 +803,11 @@ struct ImportTextProcedureWorkInstance
                 string prevHeader;
                 while(true) {
                     std::getline(stream, header);
+
+                    if (prevHeader.empty()
+                        && config.skipLineRegex.initialized()
+                        && RE2::FullMatch(header, skipLineRegex))
+                        continue;
 
                     if(!prevHeader.empty()) {
                         prevHeader += ' ' + header;
@@ -987,6 +996,8 @@ struct ImportTextProcedureWorkInstance
                                 std::vector<std::pair<ColumnPath, CellValue> > extra)>
             specializedRecorder;
 
+            std::unique_ptr<RE2> skipLineRegex;
+
         };
 
         PerThreadAccumulator<ThreadAccum> accum;
@@ -1042,6 +1053,20 @@ struct ImportTextProcedureWorkInstance
                     << " CPUs";
             }
 #endif
+
+            auto & threadAccum = accum.get();
+
+
+            // Skip lines if we are asked to
+            if (config.skipLineRegex.initialized()) {
+                if (!threadAccum.skipLineRegex) {
+                    threadAccum.skipLineRegex.reset(new RE2(config.skipLineRegex.surface().rawData()));
+                }
+                
+                if (RE2::FullMatch(re2::StringPiece(line, length),
+                                   *threadAccum.skipLineRegex))
+                    return true;
+            }
 
             // MLDB-1111 empty lines are treated as error
             if (length == 0)
@@ -1103,8 +1128,6 @@ struct ImportTextProcedureWorkInstance
                     .coerceToTimestamp().toTimestamp();
 
             //ExcAssert(!(isIdentitySelect && outputColumnNamesUnknown));
-
-            auto & threadAccum = accum.get();
 
             if (isIdentitySelect) {
                 // If it's a select *, we don't really need to run the
