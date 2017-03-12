79406d1e39163aa9b987075ed99028a26f33f416
Jeremy Barnes <jeremy@datacratic.com>
2017-03-12 21:08:48 +0000

    Optimize ExpressionValue initialization from flattened with duplicates (MLDB-dddd)

--- sql/expression_value.cc
+++ sql/expression_value.cc
@@ -1719,7 +1719,10 @@ ExpressionValue(RowValue row) noexcept
     // Structure a flattened representation.  The range between first
     // and last must all start with the same prefix, of length at least
     // level.
-    std::function<Structured (RowValue::iterator first,
+    //
+    // Returns the flattened representation, and a bool telling whether
+    // or not there are duplicate values in it.
+    std::function<std::pair<Structured, bool> (RowValue::iterator first,
                               RowValue::iterator last,
                               size_t level)>
         doLevel = [&] (RowValue::iterator first,
@@ -1749,6 +1752,8 @@ ExpressionValue(RowValue row) noexcept
             
             rowOut.reserve(numUnique);
 
+            bool hasDuplicates = numUnique < std::distance(first, last);
+            
             for (auto it = first;  it != last;  /* no inc */) {
                 if (std::get<0>(*it).size() == level + 1) {
                     // This is a final value.
@@ -1766,18 +1771,21 @@ ExpressionValue(RowValue row) noexcept
                        == std::get<0>(*it).at(level))
                     ++it2;
 
-                Structured newValue = doLevel(it, it2, level + 1);
+                auto newValue = doLevel(it, it2, level + 1);
                 rowOut.emplace_back(std::get<0>(*it).at(level),
-                                    std::move(newValue));
+                                    std::move(newValue.first));
 
                 it = it2;
             }
 
-            return rowOut;
+            return std::make_pair(std::move(rowOut), hasDuplicates);
         };
 
-    initStructured(doLevel(row.begin(), row.end(), 0 /* level */),
-                   false /* needs sorting */, true /* has duplicates (TODO) */);
+    auto topLevel = doLevel(row.begin(), row.end(), 0 /* level */);
+
+    initStructured(std::move(topLevel.first),
+                   false /* needs sorting */,
+                   topLevel.second /* has duplicates */);
 }
 
 void
